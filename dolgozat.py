0b30f7ae844dbb66e0bc9559be520082a3161149
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2022-11-15 09:38:20 +0100

    6F

--- dolgozat.py
+++ dolgozat.py
@@ -9,21 +9,30 @@ print(list)
 atlagm= sum(list)/len(list)
 tizedes= atlagm
 print("Az osztály átlagmagassága: ",atlagm,"cm")
+
 #2
 print("Legmagasabb tanuló: ",max(list),"cm")
 
+
 #3
 kul= max(list) - min(list)
+
 
 #4
 torony= sum(list)
 print("Egymás válára állva a tanulók magassága: ",torony,"m")
 
+
 #5
 #osztaly= list + 182
 #print("Osztály átlag az évközben érkezett tanulóval: ",sum(osztaly)/len(osztaly))
 
+
 #6
-print(list.append)
+list.sort()
+list.reverse()
+print(list)
+
 
 #7
+
