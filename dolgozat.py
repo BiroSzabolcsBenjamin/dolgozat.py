6c3d76d52cbd32e527d7239fb18c049e690110d2
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2022-11-15 10:22:37 +0100

    5f

--- dolgozat.py
+++ dolgozat.py
@@ -1,5 +1,5 @@
 import random
-import math
+
 
 list=[]
 
@@ -7,6 +7,7 @@ for i in range(27):
     list.append(random.randint(160,200))
 
 print(list)
+
 #1
 atlagm= sum(list)/len(list)
 tizedes= atlagm
@@ -27,14 +28,16 @@ print("Egymás válára állva a tanulók magassága: ",torony,"m")
 
 
 #5
-#osztaly= list + 182
-
+ujtanulo=[182]
+ujosztaly= list + ujtanulo
+print("Új osztály magasság: ", ujosztaly)
 
 #6
-list.sort()
-list.reverse()
-print("Tornasorba rendezve: ",list)
+ujosztaly.sort()
+ujosztaly.reverse()
+print("Tornasorba rendezve: ",ujosztaly)
 
 #7
 
-print ()
+
+
